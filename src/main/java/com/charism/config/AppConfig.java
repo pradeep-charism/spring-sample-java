37837f342eb9a54c69a7bc7ac6f804ec35c1f8fd
pradeep-charism <pradeep.charism@gmail.com>
2017-01-08 21:12:07 +0800

    demonstrate property usage

--- src/main/java/com/charism/config/AppConfig.java
+++ src/main/java/com/charism/config/AppConfig.java
@@ -7,13 +7,21 @@ import com.charism.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Created by Pradeep on 1/8/2017.
  */
 @Configuration
 @ComponentScan ({"com.charism"})
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     /*@Bean (name = "customerService")
     public CustomerService getCustomerService(){
--- src/main/java/com/charism/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/charism/repository/HibernateCustomerRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.charism.repository;
 
 import com.charism.model.Customer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,8 +14,12 @@ import java.util.List;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUser}")
+    private String dbUser;
+
     @Override
     public List<Customer> findAll(){
+        System.out.println("DB User connecting is: "+dbUser);
         List<Customer> customers = new ArrayList<>();
         customers.add(new Customer("Pradeep", "Kumar"));
         return customers;
